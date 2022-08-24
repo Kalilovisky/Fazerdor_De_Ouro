6e1e1f1a1c8008bafdf1d61ad72c1ba35093bbcd
Kalil Bispo <73958201+Kalilovisky@users.noreply.github.com>
2022-08-24 18:58:08 -0300

    Update telegram.py

--- telegram.py
+++ telegram.py
@@ -5,3 +5,17 @@ import json
 
 from telethon import TelegramClient
 from telethon.errors import SessionPasswordNeededError
+
+def readingConfigs():
+# Reading Configs
+    config = configparser.ConfigParser()
+    config.read("config.ini")
+
+# Setting configuration values
+    api_id = config['Telegram']['api_id']
+    api_hash = config['Telegram']['api_hash']
+
+    api_hash = str(api_hash)
+
+    phone = config['Telegram']['phone']
+    username = config['Telegram']['username']
